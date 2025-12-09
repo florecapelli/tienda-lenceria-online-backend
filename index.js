a1354ac4eb83b85c6ea45431daf56f90fcb802b9
florecapelli <119015963+florecapelli@users.noreply.github.com>
2025-12-09 14:54:46 -0300

    Fix estructura vercel + api/index.js

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
-import express from "express"
-import cors from "cors"
-import { configDotenv } from "dotenv"
-import rutasLog from "./src/routes/auth.routes.js"
-import rutasProductos from "./src/routes/products.routes.js"
+import express from "express";
+import cors from "cors";
+import rutasLog from "./src/routes/auth.routes.js";
+import rutasProductos from "./src/routes/products.routes.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 const corsConfig = {
-    origin: ['http://localhost:3000'], 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],                  
-    allowedHeaders: ['Content-Type', 'Authorization'],         
-    exposedHeaders: ['Content-Length'],                         
-    credentials: true,                                          
-    maxAge: 600,                                                
-    optionsSuccessStatus: 204                                   
-}
+    origin: [
+        "http://localhost:3000",
+        "https://tienda-lencer-a-online.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Content-Length"],
+    credentials: true,
+    maxAge: 600,
+    optionsSuccessStatus: 204
+};
 
-app.use(cors(corsConfig))
-app.use(express.json())
+app.use(cors(corsConfig));
+app.use(express.json());
 
-app.use("/api", rutasLog)
+app.use("/api", rutasLog);
 
 app.use((req, res, next) => {
-    console.log(`Datos received at:  ${req.method} ${req.url}`);
+    console.log(`Datos received at: ${req.method} ${req.url}`);
     next();
 });
 
-app.use("/api", rutasProductos)
+app.use("/api", rutasProductos);
 
 app.use((req, res, next) => {
-    res.status(404).send('Recurso no encontrado o ruta inválida');
+    res.status(404).send("Recurso no encontrado o ruta inválida");
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:3000:${PORT}`)
-})
+export default app;  // <<< IMPORTANTE: no usar app.listen()
