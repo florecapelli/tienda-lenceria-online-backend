c11b76a6493e044847e181e5dddff4505f9e27bd
florecapelli <119015963+florecapelli@users.noreply.github.com>
2025-12-09 15:27:23 -0300

    Agrego api/index.js y estructura correcta

--- api/index.js
+++ api/index.js
@@ -1,5 +1,33 @@
-import app from "../index.js";
+import express from "express";
+import cors from "cors";
+import rutasLog from "../src/routes/auth.routes.js";
+import rutasProductos from "../src/routes/products.routes.js";
 
-export default function handler(req, res) {
-  return app(req, res);
-}
+const app = express();
+
+const corsConfig = {
+    origin: [
+        "http://localhost:3000",
+        "https://tienda-lencer-a-online.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Content-Length"],
+    credentials: true,
+    maxAge: 600,
+    optionsSuccessStatus: 204
+};
+
+app.use(cors(corsConfig));
+app.use(express.json());
+
+// Rutas
+app.use("/api", rutasLog);
+app.use("/api", rutasProductos);
+
+// 404
+app.use((req, res) => {
+    res.status(404).json({ error: "Ruta no encontrada" });
+});
+
+export default app;
